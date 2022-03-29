eb328438737bab61dc75cb15b97557a9b58628c3
Andre Cruz <mariliaeandre.iba@hotmail.com>
2022-03-29 19:48:39 -0300

    SellerDaoJDBC and DaoFactory

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import model.Entities.Department;
 import model.Entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
     public static void main(String[] args) {
@@ -11,6 +13,9 @@ public class Program {
         System.out.println(obj);
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
