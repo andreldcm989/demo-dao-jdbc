563c7f240db91a59fbcba00342e8217762364dd7
Andre Cruz <mariliaeandre.iba@hotmail.com>
2022-03-29 20:16:24 -0300

    findById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,34 +1,77 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
+import model.Entities.Department;
 import model.Entities.Seller;
 import model.dao.SellerDao;
 
 public class SellerDaoJDBC implements SellerDao {
 
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void insert(Seller obj) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void update(Seller obj) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void deleteById(Integer id) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public Seller findById(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE seller.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {// se o ResultSet estiver vazio, a função retorna nulo. Senão, ele instancia os
+                            // objetos na memoria
+                Department dep = new Department();// Instancia o departamento
+                dep.setId(rs.getInt("DepartmentId"));// acessa o result set e pega o departmentId e aloca no
+                                                     // departamento
+                dep.setName(rs.getString("DepName")); // acessa o result set e pega o nome e aloca no departamento
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setDepartment(dep);// pega o departamento inteiro, não só o id
+                return obj;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
@@ -36,5 +79,5 @@ public class SellerDaoJDBC implements SellerDao {
         // TODO Auto-generated method stub
         return null;
     }
-    
+
 }
